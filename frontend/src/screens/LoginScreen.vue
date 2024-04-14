96aa896035c1edb98687ca1d76de8e3b8d5c8237
Simon Widmann <siwidmann@stud.hs-heilbronn.de>
2024-04-14 12:10:12 +0200

    new file:   frontend/src/components/HLoginForm.vue modified:   frontend/src/screens/LoginScreen.vue

--- frontend/src/screens/LoginScreen.vue
+++ frontend/src/screens/LoginScreen.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-
+import HLoginForm from '../components/HLoginForm.vue'
 </script>
 
 <template>
-<h1>Login</h1>
-<p>login</p>
+<h1>Register for Hitstar</h1>
+<HLoginForm>Login</HLoginForm>
 <input type="text" class="border-2 border-gray-500">
 <input type="password" name="" id="">
 </template>
