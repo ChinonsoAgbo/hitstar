2815cb5638c2d4a365b769a961d4292b8d054908
Chinonso Agbo <agbochinonso7@gmail.com>
2024-04-14 19:56:10 +0200

    GameSettingsDone

---
+++ frontend/src/components/gameSettingComponents/Genre.vue
@@ -0,0 +1,130 @@
+
+
+<template> 
+
+
+
+
+<h3 class="mb-5 text-lg font-semibold text-gray-900 dark:text-Grey">Choose Genre </h3>
+
+<ul class="grid w-full gap-4 md:grid-cols-4">
+    <li>
+        <input type="checkbox" id="rockMetal" value="" class="hidden peer">
+        <label for="rockMetal"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold">Rock / Metal </div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="electronic" value="" class="hidden peer">
+        <label for="electronic"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Electronic </div>
+
+            </div>
+        </label>
+    </li>
+    <li>
+        <input type="checkbox" id="Jazz" value="" class="hidden peer">
+        <label for="Jazz"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Jazz </div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="rb" value="" class="hidden peer">
+        <label for="rb"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> R&B</div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="Funk" value="" class="hidden peer">
+        <label for="Funk"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Funk</div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="blues" value="" class="hidden peer">
+        <label for="blues"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Blues </div>
+            </div>
+        </label>
+    </li>
+    <li>
+        <input type="checkbox" id="hipHopRap" value="" class="hidden peer">
+        <label for="hipHopRap"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> HipHop/Rap</div>
+            </div>
+        </label>
+    </li>
+    <li>
+        <input type="checkbox" id="pop" value="" class="hidden peer">
+        <label for="pop"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Pop</div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="country" value="" class="hidden peer">
+        <label for="country"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Country</div>
+            </div>
+        </label>
+    </li>
+
+    <li>
+        <input type="checkbox" id="reggae" value="" class="hidden peer">
+        <label for="reggae"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Reggae</div>
+            </div>
+        </label>
+    </li>
+    <li>
+        <input type="checkbox" id="soulMusic" value="" class="hidden peer">
+        <label for="soulMusic"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold"> Soul music</div>
+            </div>
+        </label>
+    </li>
+    <li>
+        <input type="checkbox" id="classical" value="" class="hidden peer">
+        <label for="classical"
+            class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold">Classical </div>
+            </div>
+        </label>
+    </li>
+
+</ul>
+
+
+</template>
---
+++ frontend/src/components/gameSettingComponents/SearchInput.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { reactive } from 'vue'
+
+const searchTerm = reactive({
+  query: '',
+  timeout: null,
+  results: null
+})
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout)
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query !== '') {
+
+
+    } else {
+      searchTerm.results = null
+    }
+  }, 1000)
+}
+
+</script>
+
+<template>
+  <div>
+    <!-- search field -->
+
+    <form>
+      <h3 class="mb-5 text-lg font-semibold text-gray-900 dark:text-Grey">Search Playlist </h3>
+
+      <div class="bg-white border border-indigo-600/30 rounded-lg shadow-lg flex items-center">
+       
+        <i class="fa-solid fa-magnifying-glass p-2 text-indigo-600"></i>
+        <input
+          type="text"
+          placeholder="Search for playlist"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
+       
+          v-model="searchTerm.query"
+          @input="handleSearch"
+        />
+      </div>
+    </form>
+    <!-- search suggestions -->
+    <div class="bg-white my-2 rounded-lg shadow-lg">
+        <div   >
+          <button
+            
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
+          >
+            {{ searchTerm.query }} 
+          </button>
+        </div>
+      
+    </div>
+  </div>
+</template>
