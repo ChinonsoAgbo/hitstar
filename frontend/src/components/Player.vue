abc160754c7fe47433541179cec5aff879154b77
Chinonso Agbo <agbochinonso7@gmail.com>
2024-04-21 17:58:29 +0200

    Complete update for game setting screen

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -19,8 +19,10 @@ defineProps<{
 <div  class="relative">
 
 <HAvatar :url="player.icon"> </HAvatar>
+<!-- draw avater  -->
 <span
-class="absolute bottom-0 left-8 transform translate-y-1/4 w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+class="absolute bottom-0 left-8 transform translate-y-1/4 w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full">
+</span>
 <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">
 {{ player.name }}                  
 </span>
--- frontend/src/screens/GameSettingsScreen.vue
+++ frontend/src/screens/GameSettingsScreen.vue
@@ -1,71 +1,127 @@
-<script setup>
+<script setup lang="ts">
 import SearchInput from "../components/gameSettingComponents/SearchInput.vue"
 import Genre from "../components/gameSettingComponents/Genre.vue"
-import MusicYear from "../components/gameSettingComponents/MusicYear.vue"
+
+import {ref} from 'vue'
 
 import GameLogo from "/src/components/GameLogo.vue"
 import HButton from "../components/HButton.vue";
 import RangYearSlider from "../components/gameSettingComponents/RangYearSlider.vue";
 
+
+interface Genre {
+  id: String,
+  name: string
+}
+
+// Objects of Genre
+const genres = ref<Genre[]>([
+      {
+        id: "rock", name: "Rock"
+      },
+      {
+        id: "metal", name: "Metal"
+      },
+      {
+        id: "electronic", name: "Electronic"
+      },
+
+      {
+        id: "jazz", name: "Jazz"
+      },
+      {
+        id: "r&b", name: "R&B"
+      },
+      {
+        id: "funk", name: "Funk"
+      },
+      {
+        id: "blues", name: "Blues"
+      },
+      {
+        id: "hipHopRap", name: "HipHop/Rap"
+      },
+      {
+        id: "pop", name: "Pop"
+      },
+      {
+        id: "country", name: "Country"
+      },
+      {
+        id: "Reggae", name: "Soul music"
+      },
+      {
+        id: "classical", name: "Classical"
+      },
+      {
+        id: "afroBeat", name: "Afro Beat"
+      },
+
+    ]
+);
+
 </script>
 
 <template>
-    <div class="bg-primary-300 ">
+  <div class="bg-primary-300 ">
 
 
-        <h1>Music configuration</h1>
+    <h1>Music configuration</h1>
 
-        <GameLogo> </GameLogo>
-
-
-
-        <!-- <div class="container mx-auto  "> -->
-
-        <div class="  container  flex flex-col gab-4 relative space-y-5   mx-auto min-h-screen   py-6 sm:py-12">
-            <div class="" > <p> Configured music: 0 </p> </div>
-            <!-- ... -->
-
-            <!-- Search -->
-            <div>
-               <SearchInput> </SearchInput>
-            </div>
+    <GameLogo></GameLogo>
 
 
-            <div class="mt-10  mb-10">
+    <!-- <div class="container mx-auto  "> -->
 
-                <!-- Genre -->
-                <Genre> </Genre>
-            </div>
-            <!-- music year -->
+    <div class="  container  flex flex-col gab-4 relative space-y-5   mx-auto min-h-screen   py-6 sm:py-12">
+      <div class="">
+        <p> Configured music: 0 </p>
+      </div>
+      <!-- ... -->
 
-            <div>
-                <h3 class="mb-5 text-lg font-semibold text-gray-900 dark:text-Grey">Choose music year </h3>
-
-                <RangYearSlider> </RangYearSlider>
-
-            </div>
-
-            <!-- Button to submit -->
-            <div class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 ">
+      <!-- Search -->
+      <div>
+        <SearchInput></SearchInput>
+      </div>
 
 
-                <RouterLink class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 " to="/qr-code">
+      <div class="mt-10  mb-10">
 
-                    <HButton class="bg-red-700 hover:bg-red-800 "> Undo </HButton>
-                </RouterLink>
-                <RouterLink class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 " to="/qr-code">
+        <!-- Genre -->
+        <h3 class="mb-5 text-lg font-semibold text-gray-900 dark:text-Grey">Choose Genre </h3>
 
-                    <HButton> Save </HButton>
-                </RouterLink>
+        <ul class="grid w-full gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
 
-
-
-            </div>
+          <Genre v-for="genre in genres" :genre="genre"></Genre>
+        </ul>
 
 
+      </div>
+      <!-- music year -->
 
-        </div>
+      <div>
+        <h3 class="mb-5 text-lg font-semibold text-gray-900 dark:text-Grey">Choose music year </h3>
+
+        <RangYearSlider></RangYearSlider>
+
+      </div>
+
+      <!-- Button to submit -->
+      <div class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 ">
+        <RouterLink class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 " to="/qr-code">
+          <HButton class="bg-red-700 hover:bg-red-800 "> Undo</HButton>
+        </RouterLink>
+
+        <RouterLink class="relative mt-10  mb-10 z-0 w-full group grid grid-cols-2 " to="/qr-code">
+          <HButton> Save</HButton>
+        </RouterLink>
+
+
+      </div>
+
 
     </div>
+
+  </div>
 
 </template>
