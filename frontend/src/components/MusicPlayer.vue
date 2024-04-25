2aed5680a181d68add8c781cec629b048fbb1a19
Cornelius Hartmann <chartmann@stud.hs-heilbronn.de>
2024-04-25 12:16:14 +0200

    implemented playable song in MusicPlayer.vue

--- frontend/src/components/MusicPlayer.vue
+++ frontend/src/components/MusicPlayer.vue
@@ -1,20 +1,44 @@
 <script setup lang="ts">
-import {PauseIcon, PlayIcon} from "@heroicons/vue/24/outline";
+import {PauseIcon, PlayIcon, ArrowPathIcon} from "@heroicons/vue/24/outline";
 import {onMounted, ref, watch} from 'vue'
 import { useAnimate } from '@vueuse/core'
 import { useSound } from '@vueuse/sound'
-// import trumpetSfx from '../assets/sounds/fanfare.mp3'
+import aveMaria from '../assets/music/Ave_Maria.mp3'
 
 const props = defineProps<{
   timeDelta: number
 }>();
 
+const isLoading = ref(true);
+const bar = ref()
+const animation = useAnimate(
+    bar,
+    [
+        { width: '0%' },
+        { width: '100%' }
+    ],
+    props.timeDelta
+)
+
+
+const sound = useSound(aveMaria, {
+  volume: 1.0,
+  autoplay: true,
+  soundEnabled: true,
+  onload: () => {
+    isLoading.value = false;
+    sound.play();
+    animation.play();
+  }
+});
+
 const emit = defineEmits<{
   (event: 'finished'): void
 }>();
-// const sound = useSound(trumpetSfx)
-const bar = ref()
-const animation = useAnimate(bar, [{ width: '0%' }, { width: '100%' }], props.timeDelta)
+
+onMounted(() => {
+  animation.pause();
+})
 
 watch(animation.playState, () => {
   if (animation.playState.value === 'finished') {
@@ -27,31 +51,19 @@ watch(animation.playState, () => {
   <div class="w-80 h-80 p-5 bg-white rounded-xl flex flex-col justify-center items-center border-4 border-secondary-500">
 
     <PauseIcon
-        v-if="animation.playState.value === 'running'"
-        @click="animation.pause()"
+        v-if="animation.playState.value === 'running' && !isLoading"
+        @click="() => {animation.pause();sound.pause()}"
         class="w-20 h-20 cursor-pointer" />
 
     <PlayIcon
-        v-else
-        @click="animation.play()"
+        v-else-if="!isLoading"
+        @click="() => {animation.play();sound.play()}"
         class="w-20 h-20 cursor-pointer" />
 
-    <div class="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
+    <ArrowPathIcon v-if="isLoading" class="w-5 h-5 animate-spin"/>
+
+    <div v-if="!isLoading" class="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
         <div ref="bar" class="h-6 bg-primary-600 rounded-full dark:bg-primary-500"></div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.play-leave-active, .play-enter-active {
-  animation: slide-in 5s linear;
-}
-@keyframes slide-in {
-  0% {
-    width: 0;
-  }
-  100% {
-    width: 100%;
-  }
-}
-</style>
