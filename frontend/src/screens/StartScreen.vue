b6e6204dd935827b11bf4a0b81fb387fe4a6f9c0
Chinonso Agbo <agbochinonso7@gmail.com>
2024-04-28 17:15:05 +0200

    test

--- frontend/src/screens/StartScreen.vue
+++ frontend/src/screens/StartScreen.vue
@@ -9,148 +9,120 @@ import { base64encode, codeVerifier, generateCodeChallenge, sha256 } from '../au
 //const codeVerifier = generateRandomString(64);
 
 const isLoggedIn = ref(true);
+const changeLoginStatus = () => {
+  isLoggedIn.value = !isLoggedIn.value;
+
+}
+
 
 const client_id = 'b7fbf387ec2346fe810dea140d435788';
-const redirect_uri = 'http://localhost:5173/';
+// const redirect_uri = 'http://localhost:5173/';
 
-const getToken = async (code: string) => {
-    try {
-        const formData = new URLSearchParams({
-            client_id,
-            grant_type: 'authorization_code',
-            code,
-            redirect_uri,
-            // add any additional parameters you need
-        });
-
-        const response = await axios.post('https://accounts.spotify.com/api/token', formData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        });
-
-        if (response.status === 200) {
-            const { access_token, refresh_token, expires_in, token_type, scope } = response.data;
-            localStorage.setItem('access_token', access_token);
-            localStorage.setItem('refresh_token', refresh_token);
-            localStorage.setItem('expires_in', expires_in.toString());
-            localStorage.setItem('token_type', token_type);
-            localStorage.setItem('scope', scope);
-
-            console.log('Access Token:', access_token);
-            return access_token;
-        } else {
-            console.error('Failed to get access token:', response.data);
-            return null;
-        }
-    } catch (error) {
-        console.error('Error getting access token:', error);
-        return null;
-    }
-};
-
-const refreshAccessToken = async ()=> {
-    try {
-        const refreshToken = localStorage.getItem('refresh_token');
-        if (!refreshToken) {
-            console.error('Refresh token not found');
-            return null;
-        }
-        const formData = new URLSearchParams({
-            grant_type: 'refresh_token',
-            refresh_token: refreshToken,
-            client_id: client_id,
-            // add any additional parameters you need
-        });
-
-        const response = await axios.post('https://accounts.spotify.com/api/token', formData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        });
-
-        if (response.status === 200) {
-            const { access_token, refresh_token, expires_in, token_type, scope } = response.data;
-            localStorage.setItem('access_token', access_token);
-            localStorage.setItem('refresh_token', refresh_token);
-            localStorage.setItem('expires_in', expires_in.toString());
-            localStorage.setItem('token_type', token_type);
-            localStorage.setItem('scope', scope);
-
-            console.log('Access TokenRefresh:', access_token);
-            return access_token;
-        } else {
-            console.error('Failed to refresh access token:', response.data);
-            return null;
-        }
-    } catch (error) {
-        console.error('Error refreshing access token:', error);
-        return null;
-    }
-};
-
-const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-    const expiresIn = localStorage.getItem('expires_in');
-    const expirationTime = expiresIn ? parseInt(expiresIn) * 1000 : 0; // Convert to milliseconds
-    const currentTime = new Date().getTime();
-
-    if (!accessToken || expirationTime < currentTime) {
-        // Access token doesn't exist or is expired, fetch a new one
-        const code = new URLSearchParams(window.location.search).get('code');
-        if (code) {
-            return await getToken(code);
-        } else {
-            console.error('Authorization code not found.');
-            return null;
-        }
-    } else {
-        // Access token exists and is not expired
-        return accessToken;
-    }
-};
-
-const checkAndRefreshAccessToken = async ()=> {
-    const expiresIn = localStorage.getItem('expires_in');
-    const expirationTime = expiresIn ? parseInt(expiresIn) * 1000 : 0; // Convert to milliseconds
-    const currentTime = new Date().getTime();
-
-    if (expirationTime > 0 && expirationTime - currentTime < 60000) {
-        // If token is about to expire or already expired, refresh it
-        console.log('Access token is expired or about to expire. Refreshing...');
-        return await refreshAccessToken();
-    }
+const fetchProfile = async (token: string): Promise<any> => {
+  try {
+    const result = await axios.get("https://api.spotify.com/v1/me", {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    return result.data;
+  } catch (error) {
+    console.error('Error fetching profile:', error);
     return null;
+  }
 };
 
-// Initialize the application
-const initializeApp = async () => {
-    const accessToken = await getAccessToken();
-    if (accessToken) {
-        // Application initialized successfully, do whatever you need to do
-        console.log('Application initialized successfully.');
-    } else {
-        console.error('Failed to initialize application.');
+const fetchProfileAndPlaylists = async (token: string): Promise<{ profile: any, playlists: any }> => {
+    try {
+        const profileResponse = await axios.get("https://api.spotify.com/v1/me", {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        const playlistsResponse = await axios.get("https://api.spotify.com/v1/me/playlists", {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        const profile = profileResponse.data;
+        const playlists = playlistsResponse.data;
+
+        return { profile, playlists };
+    } catch (error) {
+        console.error('Error fetching profile and playlists:', error);
+        return { profile: null, playlists: null };
     }
 };
 
-initializeApp();
 
-setInterval(checkAndRefreshAccessToken, 60000); // Check every minute and refresh if necessary
+
+// Function to refresh the access token 
+const refreshAccessToken = async () => {
+  try {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (!refreshToken) {
+      console.error('Refresh token not found');
+      return;
+    }
+
+    const formData = new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      client_id: client_id
+    });
+
+    const response = await axios.post('https://accounts.spotify.com/api/token', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    if (response.status === 200) {
+      const { access_token, refresh_token, expires_in, token_type, scope } = response.data;
+      localStorage.setItem('access_token', access_token);
+      localStorage.setItem('refresh_token', refresh_token);
+      localStorage.setItem('expires_in', expires_in.toString());
+      localStorage.setItem('token_type', token_type);
+      localStorage.setItem('scope', scope);
+      console.log('Access token refreshed:', access_token);
+      // Fetch user profile after refreshing the access token
+      fetchProfileAndPlaylists(access_token)
+    .then(({ profile, playlists }) => {
+        console.log('Profile:', profile);
+        console.log('Playlists:', playlists);
+    })
+        .catch(error => {
+          console.error('Error fetching profile:', error);
+        });
+    } else {
+      console.error('Failed to refresh access token:', response.data);
+    }
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+  }
+};
+// Check if the access token is expired and refresh if necessary 
+const checkAndRefreshAccessToken = () => {
+  const expiresIn = localStorage.getItem('expires_in');
+  const expirationTime = expiresIn ? parseInt(expiresIn) * 1000 : 0; // Convert to milliseconds
+  const currentTime = new Date().getTime();
+
+  if (expirationTime > 0 && expirationTime - currentTime < 60000) {
+    // If token is about to expire or already expired, refresh it
+    console.log('Access token is expired or about to expire. Refreshing...');
+    refreshAccessToken();
+  }
+};
+
+// Call checkAndRefreshAccessToken
+checkAndRefreshAccessToken();
+// Set interval to call checkAndRefreshAccessToken every minute
+setInterval(checkAndRefreshAccessToken, 60000);
+
+
+
 </script>
 
 <template>
   <div @click="changeLoginStatus" class="absolute top-5 right-5 h-16 w-16">
     <HAvatar url="/profile-picture-5.jpg"> </HAvatar>
   </div>
-  <div
-    class="flex flex-col items-center justify-center space-y-3s bg-primary-300 min-h-screen"
-  >
-    <img
-      class="rounded-full w-96 h-96, flex justify-center, align-middle"
-      src="/hitstar.jpg"
-      alt="image description"
-    />
+  <div class="flex flex-col items-center justify-center space-y-3s bg-primary-300 min-h-screen">
+    <img class="rounded-full w-96 h-96, flex justify-center, align-middle" src="/hitstar.jpg" alt="image description" />
 
     <div class="items-center space-y-3 grid-cols-2 gap-4">
       <RouterLink v-show="!isLoggedIn" to="/login">
