d1c5619674bd018d24213c94dc42f99e9b614db1
Cornelius Hartmann <chartmann@stud.hs-heilbronn.de>
2024-05-04 18:08:26 +0200

    Removed git bugs in HCard.vue

--- frontend/src/components/HCard.vue
+++ frontend/src/components/HCard.vue
@@ -8,26 +8,9 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-<<<<<<< HEAD
-    <!-- <vue-flip active-click class="w-80 h-80"> -->
-        <!-- <template #front>
-        <img class="rounded-2xl w-80 h-80" src="../public/hitstar.jpg" alt="" />
-        </template> -->
-        <!-- <template #back> -->
-
-            <div
-                :style="{ width: size + 'em', height: size + 'em', padding: padding ? '1.25rem' : '0rem' }"
-                class="flex-col bg-secondary-500 border-primary-700 border-4 rounded-2xl">
-                <slot />
-            </div>
-        <!-- </template> -->
-    <!-- </vue-flip> -->
-
-=======
     <div
         :style="{ width: size + 'em', height: size + 'em', padding: padding ? '1.25rem' : '0rem' }"
         class="flex-col bg-secondary-500 border-primary-700 border-4 rounded-2xl">
         <slot />
     </div>
->>>>>>> feature/game-screen
 </template>
