da6355c7ee8af3b422024ec1ee385da7588beb91
Michael Wenninger <mwenninger@stud.hs-heilbronn.de>
2024-05-15 00:52:12 +0200

    added a small Rest-Example to the BackEnd

--- backend/src/main/java/com/example/hitstar_backend/HitstarBackendApplication.java
+++ backend/src/main/java/com/example/hitstar_backend/HitstarBackendApplication.java
@@ -2,36 +2,44 @@ package com.example.hitstar_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.UUID;
+import java.util.HashSet;
 
 
 @SpringBootApplication
 public class HitstarBackendApplication {
-    private HashMap<String, String> sessions = new HashMap<>();
 
-    @PostMapping("/session/")
-    public String createGameSession(String sessionId) {
-        String session = UUID.randomUUID().toString();
-        sessions.put(sessionId, session);
-        return session;
+    @RestController
+    class HistarController {
+        private HashMap<String, HashSet<String>> sessionUsers = new HashMap<>();
+
+        @PostMapping("/session/{sessionId}/user/{user}")
+        public String addUserToSession(@PathVariable String sessionId, @PathVariable String user) {
+
+            HashSet<String> users = sessionUsers.get(sessionId);
+
+
+            if (users == null) {
+                users = new HashSet<>();
+                sessionUsers.put(sessionId, users);
+            }
+
+            users.add(user);
+
+            return user;
+        }
+
+        @GetMapping("/session/{sessionId}/user/")
+        public HashSet<String> getUsersInSession(@PathVariable String sessionId) {
+            return sessionUsers.get(sessionId);
+        }
     }
 
-    @GetMapping("/session/{id}")
-    public String getSessionId(@PathVariable String id) {
-
-        return id;
-    }
-
-
-    PostMapping("/user/")
-    public String saveUser(String userId){
-
-    }
     public static void main(String[] args) {
         SpringApplication.run(HitstarBackendApplication.class, args);
 
