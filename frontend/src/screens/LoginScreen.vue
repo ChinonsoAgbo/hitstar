07d87522a9727aee24e159e65502eed3001f87c8
Simon Widmann <siwidmann@stud.hs-heilbronn.de>
2024-05-16 03:21:45 +0200

    Finished User Story

--- frontend/src/screens/LoginScreen.vue
+++ frontend/src/screens/LoginScreen.vue
@@ -1,36 +1,49 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import HLoginForm from '../components/HLoginForm.vue'
 import HSubmitButton from '../components/HSubmitButton.vue'
 import HButton from '../components/HButton.vue'
 import HCard from '../components/HCard.vue'
 
+const email = ref('')
 const password = ref('')
 
-const isPasswordEmpty = computed(() => {
-  return password.value === ''
+const isFormValid = computed(() => {
+  return email.value !== '' && password.value !== ''
 })
+
 </script>
 
 <template>
   <div class="bg-neutral">
     <h1 class="mb-5 ml-5 font-bold">Login for your Hitstar account</h1>
-    <HLoginForm>Login</HLoginForm>
-    <div class="mb-5 max-w-sm ml-5">
-      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-      <input
-          type="password"
-          id="password"
-          v-model="password"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-      />
-    </div>
+    <form class="max-w-sm ml-5">
+      <div class="mb-5">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
+        <input
+            type="email"
+            id="email"
+            v-model="email"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@hitstar.com"
+            required
+        />
+      </div>
+      <div class="mb-5">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+        <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+        />
+      </div>
+      <p v-if="!isFormValid" class="text-red-500 text-sm mt-1">Please fill all fields.</p>
+      <RouterLink to="/start">
+        <HSubmitButton :disabled="!isFormValid">Login</HSubmitButton>
+      </RouterLink>
+    </form>
     <RouterLink to="/start">
       <HButton>Go back to Start Screen</HButton>
-    </RouterLink>
-    <RouterLink to="/start">
-      <HSubmitButton :disabled="isPasswordEmpty">Login</HSubmitButton>
     </RouterLink>
     <HCard size=15 :padding='false'>
       <img class="rounded-2xl" src="/hitstar.jpg" alt="" />
