cc95e67a0cbb7c119b87ab62331b9c31f24d433e
Simon Widmann <siwidmann@stud.hs-heilbronn.de>
2024-05-19 15:39:31 +0200

    Added model class variables.

--- backend/src/main/java/de/hhn/labsw/hitstar_backend/model/AuthentificationToken.java
+++ backend/src/main/java/de/hhn/labsw/hitstar_backend/model/AuthentificationToken.java
@@ -1,20 +1,29 @@
 package de.hhn.labsw.hitstar_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "authentificationtoken")
 public class AuthentificationToken {
+
+    @Getter
+    @Setter
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Getter
+    @Setter
+    @Column(name = "token")
+    private String token;
 
-    public Long getId() {
-        return id;
-    }
+    @Getter
+    @Setter
+    @Column(name="expirydate")
+    @DateTimeFormat
+    private Long expiryDate;
 }
