3f97c52b067ec5dbf35b8b2b34bd962e13e72b7e
Simon Widmann <siwidmann@stud.hs-heilbronn.de>
2024-06-05 17:15:46 +0200

    Improved UI

--- frontend/main_device/src/screens/LoginScreen.vue
+++ frontend/main_device/src/screens/LoginScreen.vue
@@ -46,51 +46,56 @@ async function login() {
 </script>
 
 <template>
-  <div class="bg-neutral">
-    <h1 class="mb-5 ml-5 font-bold">Login for your Hitstar account</h1>
-    <form class="max-w-sm ml-5" @submit.prevent="login">
-      <div class="mb-5">
-        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+  <div class="flex flex-col items-center justify-center space-y-3 bg-primary-300 min-h-screen">
+    <h1 class="mb-5 text-xl font-bold">Login for your Hitstar account</h1>
+    <div class="flex flex-row space-x-5">
+      <form class="max-w-sm" @submit.prevent="login">
+        <div class="mb-5">
+          <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Your Username
-        </label>
-        <input
-            type="text"
-            id="username"
-            v-model="username"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg \
-                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 \
-                  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 \
-                  dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Max Mustermann"
-            required
-        />
-      </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Your password
-        </label>
-        <input
-            type="password"
-            id="password"
-            v-model="password"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
-                  focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
-                  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
-                  dark:shadow-sm-light"
-            required
-        />
-      </div>
-      <p v-if="!isFormValid" class="text-red-500 text-sm mt-1">Please fill all fields.</p>
-      <p v-if="infoText!=''" class="text-blue-500 text-sm mt-1">{{infoText}}</p>
-<!--      <RouterLink to="/start">-->
-        <HSubmitButton :disabled="!isFormValid">Login</HSubmitButton>
-<!--      </RouterLink>-->
-    </form>
-    <RouterLink to="/start">
-      <HButton>Go back to Start Screen</HButton>
-    </RouterLink>
-    <HCard size=15 :padding='false'>
-      <img class="rounded-2xl" :src="`${IMAGE_URL}hitstar.jpg`" alt="" />
-    </HCard>
+          </label>
+          <input
+              type="text"
+              id="username"
+              v-model="username"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg \
+                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 \
+                    dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 \
+                    dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Max Mustermann"
+              required
+          />
+        </div>
+        <div class="mb-5">
+          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Your Password
+          </label>
+          <input
+              type="password"
+              id="password"
+              v-model="password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
+                    focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
+                    dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
+                    dark:shadow-sm-light"
+              required
+          />
+        </div>
+        <p v-if="!isFormValid" class="text-red-500 text-sm mt-1">Please fill all fields.</p>
+        <p v-if="infoText !== ''" class="text-blue-500 text-sm mt-1">{{infoText}}</p>
+        <div class="flex justify-between">
+          <HSubmitButton
+              :disabled="!isFormValid"
+              :class="{ 'bg-gray-500 hover:bg-gray-500': !isFormValid, 'bg-blue-500 hover:bg-blue-500': isFormValid }"
+          >Login</HSubmitButton>
+          <RouterLink to="/start">
+            <HButton>Go back to Start Screen</HButton>
+          </RouterLink>
+        </div>
+      </form>
+      <HCard size=15 :padding='false'>
+        <img class="rounded-2xl" :src="`${IMAGE_URL}hitstar.jpg`" alt="" />
+      </HCard>
+    </div>
   </div>
 </template>
--- frontend/main_device/src/screens/RegisterScreen.vue
+++ frontend/main_device/src/screens/RegisterScreen.vue
@@ -49,67 +49,73 @@ async function register() {
 </script>
 
 <template>
-  <div class="bg-neutral">
-    <h1 class="mb-5 ml-5 font-bold">Register for Hitstar</h1>
-    <form class="max-w-sm ml-5" @submit.prevent="register">
-      <div class="mb-5">
-        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+  <div class="flex flex-col items-center justify-center space-y-3 bg-primary-300 min-h-screen">
+    <h1 class="mb-5 text-xl font-bold">Register for Hitstar</h1>
+    <div class="flex flex-row space-x-5">
+      <form class="max-w-sm" @submit.prevent="register">
+        <div class="mb-5">
+          <label for="username" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
             Your Username
-        </label>
-        <input
-            type="text"
-            id="username"
-            v-model="username"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
-                  focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
-                  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
-                  dark:shadow-sm-light"
-            placeholder="Max Mustermann"
-            required
-        />
-      </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-        <input
-            type="password"
-            id="password"
-            v-model="password"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
-                  focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 \
-                  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            required
-        />
-      </div>
-      <div class="mb-5">
-        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Repeat Password
-        </label>
-        <input
-            type="password"
-            id="confirm-password"
-            v-model="confirmPassword"
-            :class="{'border-red-500': !isNotEmpty, 'border-gray-300': isNotEmpty}"
-            class="shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
-                   focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
-                   dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
-                   dark:shadow-sm-light"
-            required
-        />
-        <p v-if="isPasswordMismatch" class="text-red-500 text-sm mt-1">Passwords do not match.</p>
-        <p v-if="!isNotEmpty" class="text-red-500 text-sm mt-1">Please fill all fields.</p>
-        <p v-if="!hasValidLength" class="text-red-500 text-sm mt-1">Password has to have between 5 and 50 digits. Username has to have between 3 and 20 digits.</p>
-        <p v-if="infoText!=''" class="text-blue-500 text-sm mt-1">{{infoText}}</p>
-      </div>
-<!--      <RouterLink to="/login">-->
-        <HSubmitButton :disabled="!isNotEmpty || isPasswordMismatch || !hasValidLength">Register</HSubmitButton>
-<!--      </RouterLink>-->
-    </form>
-    <RouterLink to="/start">
-      <HButton>Go back to Start Screen</HButton>
-    </RouterLink>
-    <HCard size=15 :padding='false'>
-      <img class="rounded-2xl" :src="`${IMAGE_URL}hitstar.jpg`" alt="" />
-    </HCard>
+          </label>
+          <input
+              type="text"
+              id="username"
+              v-model="username"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
+                    focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
+                    dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
+                    dark:shadow-sm-light"
+              placeholder="Max Mustermann"
+              required
+          />
+        </div>
+        <div class="mb-5">
+          <label for="password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Password</label>
+          <input
+              type="password"
+              id="password"
+              v-model="password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
+                    focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 \
+                    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              required
+          />
+        </div>
+        <div class="mb-5">
+          <label for="confirm-password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
+            Repeat Password
+          </label>
+          <input
+              type="password"
+              id="confirm-password"
+              v-model="confirmPassword"
+              :class="{'border-red-500': isPasswordMismatch, 'border-gray-300': !isPasswordMismatch}"
+              class="shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 \
+                     focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 \
+                     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 \
+                     dark:shadow-sm-light"
+              required
+          />
+          <p v-if="isPasswordMismatch" class="text-red-500 text-base mt-1">Passwords do not match.</p>
+          <p v-if="!isNotEmpty" class="text-red-500 text-base mt-1">Please fill all fields.</p>
+          <p v-if="!hasValidLength" class="text-red-500 text-base mt-1">Password has to have between 5 and 50 digits. Username has to have between 3 and 20 digits.</p>
+          <p v-if="infoText !== ''" class="text-blue-500 text-base mt-1">{{infoText}}</p>
+        </div>
+        <div class="flex justify-between">
+          <HSubmitButton
+              :disabled="!isNotEmpty || isPasswordMismatch || !hasValidLength"
+              :class="{ 'bg-gray-500 hover:bg-gray-500': !isNotEmpty || isPasswordMismatch || !hasValidLength,
+              'bg-blue-500 hover:bg-blue-500': isNotEmpty && !isPasswordMismatch && hasValidLength}"
+          >Register</HSubmitButton>
+          <RouterLink to="/start">
+            <HButton>Go back to Start Screen</HButton>
+          </RouterLink>
+        </div>
+      </form>
+      <HCard size=15 :padding='false'>
+        <img class="rounded-2xl" :src="`${IMAGE_URL}hitstar.jpg`" alt="" />
+      </HCard>
+    </div>
   </div>
 </template>
 
