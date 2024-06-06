fdc8555f2f7bb5ebc8e9970b15f579efc744c894
Chinonso Agbo <agbochinonso7@gmail.com>
2024-06-06 23:35:06 +0200

    clean ups

--- frontend/main_device/src/stores/spotifyStore.ts
+++ frontend/main_device/src/stores/spotifyStore.ts
@@ -11,41 +11,6 @@ export const useSpotifyStore = defineStore('spotify', () => {
   const current_track = ref({});
   const token = ref('')
 
-    interface Track {
-      album: {
-        album_type: string;
-        artists: [];
-        available_markets: string[];
-        external_urls: { spotify: string };
-        href: string;
-        id: string;
-        images: [];
-        name: string;
-        release_date: string;
-        release_date_precision: string;
-        total_tracks: number;
-        type: string;
-        uri: string;
-      };
-      artists: [];
-      available_markets: string[];
-      disc_number: number;
-      duration_ms: number;
-      explicit: boolean;
-      external_ids: { isrc: string };
-      external_urls: { spotify: string };
-      href: string;
-      id: string;
-      is_local: boolean;
-      name: string;
-      popularity: number;
-      preview_url: string | null;
-      track_number: number;
-      type: string;
-      uri: string;
-    }
-  
-  */
   type Playlist = {
     id: string;
     name: string;
