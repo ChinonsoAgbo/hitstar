defe8060c3f6ce87d864ae0f5c94a9faa241726e
Michael Wenninger <mwenninger@stud.hs-heilbronn.de>
2024-06-23 13:21:35 +0200

    makeing the script interactive to change the ip-adress and protocoll

--- Build_Project/build.sh
+++ Build_Project/build.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
-# Hier bite die IP-Adressen gewünschte IP Adresse eintragen
-ip_adress="hitstar.ddns.net"
-ip_adress_http="https://hitstar.ddns.net"
 
 # Beendet das Skript, wenn ein Befehl fehlschlägt
 set -e
 
-# Docker herunterfahren
+# Fragt nach dem Protokoll und der IP-Adresse, welche im nächsten Schritt verwendet wird.
+read -p "Bitte geben Sie das Protokoll ein (http oder https, Standard: https): " protocol
+read -p "Bitte geben Sie die IP-Adresse ein (für Lokal dieses Format verwenden: 192.168.178.1, Standard: hitstar.ddns.net): " ip_adress
+
+# Setzen Sie die Standardwerte, wenn keine Eingabe erfolgt, sonst werden das eingetragene Protokoll und die IP-Adresse verwendet
+ip_adress=${ip_adress:-hitstar.ddns.net}
+protocol=${protocol:-https}
+# Baut aus den oberen beiden Werten die IP-Adresse mit dem Protokoll zusammen
+ip_adress_http="$protocol://$ip_adress"
+
+
+# Docker Container die in der  compose.yml definiert sind herunterfahren und aktuell laufende Container löschen
 docker compose down
 
-# Das Spring-Boot-Projekt in builden
-#cd ../backend
-#./gradlew build -x test 
-#docker build --build-arg JAR_FILE=../build/libs/backend-0.0.1-SNAPSHOT.jar -t springio/hitstar_backend .
+# Das Spring-Boot-Projekt aus dem Backend bauen und daraus ein Docker-Container erstellen
+cd ../backend
+./gradlew build -x test 
+docker build --build-arg JAR_FILE=../build/libs/backend-0.0.1-SNAPSHOT.jar -t springio/hitstar_backend .
 
-
-# Pfad zu den .env Dateien
+# Pfad zu den .env Dateien welche für die IP-Adressen benötigt werden
 env_file_main_device="../frontend/main_device/.env"
 env_file_controller="../frontend/controller/.env"
 
-
-
-# Ändern Sie die Variable in beiden Dateien
+# Ändern Sie die Variablen in beiden Dateien
 sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_main_device
 sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_controller
 sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_main_device
 sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_controller
 
-
-
-
-# In das Verzeichnis 'frontend/main_device' wechseln und bauen
+# In das Verzeichnis 'frontend/main_device' wechseln ggf. fehlende abhängigkeiten installieren und bauen
 cd ../frontend/main_device
 npm install
 npm run build
 
-# In das Verzeichnis 'controller' wechseln und bauen
+# In das Verzeichnis 'controller' wechseln ggf. fehlende abhängigkeiten installieren und bauen
 cd ../controller
 npm install
 npm run build 
 
-# Zurück zum Hauptverzeichnis und in das Verzeichnis 'Docker' wechseln
+# Zurück zum Hauptverzeichnis und in das Verzeichnis 'Build_Project' wechseln
 cd ../../Build_Project
 
-# Docker starten
+# Docker-Container starten
 docker compose up -d
+
+echo -e "\nHerzlichen Glückwunsch! Das Projekt wurde vollständig gebaut. \nBitte überprüfen Sie ggf. in Docker Desktop, ob alle 7 Container nach 10 Sekunden noch laufen."
