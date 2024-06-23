2ed6e37af29aa3e7604939b12c764f4f12071fbe
Michael Wenninger <mwenninger@stud.hs-heilbronn.de>
2024-06-23 02:02:58 +0200

    add ip-change option in the build.sh file and  changed it to hitstar.ddns.net

--- Build_Project/build.sh
+++ Build_Project/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Hier bite die IP-Adressen gewünschte IP Adresse eintragen
+ip_adress="hitstar.ddns.net"
+ip_adress_http="https://hitstar.ddns.net"
+
 # Beendet das Skript, wenn ein Befehl fehlschlägt
 set -e
 
@@ -7,9 +11,22 @@ set -e
 docker compose down
 
 # Das Spring-Boot-Projekt in builden
-cd ../backend
-./gradlew build -x test 
-docker build --build-arg JAR_FILE=../build/libs/backend-0.0.1-SNAPSHOT.jar -t springio/hitstar_backend .
+#cd ../backend
+#./gradlew build -x test 
+#docker build --build-arg JAR_FILE=../build/libs/backend-0.0.1-SNAPSHOT.jar -t springio/hitstar_backend .
+
+
+# Pfad zu den .env Dateien
+env_file_main_device="../frontend/main_device/.env"
+env_file_controller="../frontend/controller/.env"
+
+
+
+# Ändern Sie die Variable in beiden Dateien
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_main_device
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_controller
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_main_device
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_controller
 
 
 
