d55c376f1bc3919801e5ccfd4c6f2d29f551f073
Michael Wenninger <mwenninger@stud.hs-heilbronn.de>
2024-06-24 11:12:52 +0200

    optimizing

--- Build_Project/build.sh
+++ Build_Project/build.sh
@@ -4,16 +4,45 @@
 set -e
 
 # Fragt nach dem Protokoll und der IP-Adresse, welche im nächsten Schritt verwendet wird.
+read -p "Soll das Programm im Entwicklermodus gestartet werden?(zum Out-of-the-Box testen bitte n wählen (j/n)): " dev_mode
+
+
+# Pfad zu den .env Dateien
+env_file_main_device="../frontend/main_device/.env"
+env_file_controller="../frontend/controller/.env"
+
+
+if [ "$dev_mode" = "j" ]; then
+ip_adress=localhost
+protocol=http
+port=5174
+# Baut aus den oberen beiden Werten die IP-Adresse mit dem Protokoll zusammen
+ip_adress_http="$protocol://$ip_adress"
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_main_device
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_controller
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_main_device
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_controller
+sed -i "s/^VITE_PORT=.*$/VITE_PORT=$port/" $env_file_main_device
+else
+
+# Setzen Sie die Standardwerte, wenn keine Eingabe erfolgt, sonst werden das eingetragene Protokoll und die IP-Adresse verwendet
 read -p "Bitte geben Sie das Protokoll ein (http oder https, Standard: https): " protocol
 read -p "Bitte geben Sie die IP-Adresse ein (für Lokal dieses Format verwenden: 192.168.178.1, Standard: hitstar.ddns.net): " ip_adress
 read -p "Möchten Sie das Backend bauen? (für das erste mal unbedingt j, Standard: n): " build_backend
 
-# Setzen Sie die Standardwerte, wenn keine Eingabe erfolgt, sonst werden das eingetragene Protokoll und die IP-Adresse verwendet
 ip_adress=${ip_adress:-hitstar.ddns.net}
 protocol=${protocol:-https}
 build_backend=${build_backend:-n}
+
 # Baut aus den oberen beiden Werten die IP-Adresse mit dem Protokoll zusammen
 ip_adress_http="$protocol://$ip_adress"
+port=8082
+# Ändert die IP-Adress Varialben in den .env Dateien in den beiden Frontend Projekten
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_main_device
+sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_controller
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_main_device
+sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_controller
+sed -i "s/^VITE_PORT=.*$/VITE_PORT=$port/" $env_file_main_device
 
 if [ "$build_backend" = "j" ]; then
     # Docker herunterfahren und aktuell laufende Container löschen
@@ -27,17 +56,6 @@ else
     docker stop web_controller web_main_device
     docker rm web_controller web_main_device
 fi
-
-# Pfad zu den .env Dateien
-env_file_main_device="../frontend/main_device/.env"
-env_file_controller="../frontend/controller/.env"
-
-# Ändert die IP-Adress Varialben in den .env Dateien in den beiden Frontend Projekten
-sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_main_device
-sed -i "s|^VITE_IP_ADRESS_WITH_HTTP=.*$|VITE_IP_ADRESS_WITH_HTTP=$ip_adress_http|" $env_file_controller
-sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_main_device
-sed -i "s/^VITE_IP_ADRESS=.*$/VITE_IP_ADRESS=$ip_adress/" $env_file_controller
-
 # In das Verzeichnis 'frontend/main_device' wechseln und bauen
 cd ../frontend/main_device
 npm install
@@ -55,3 +73,11 @@ cd ../../Build_Project
 docker compose up -d
 
 echo -e "\nHerzlichen Glückwunsch! Das Projekt wurde vollständig gebaut, bitte überprüfen Sie ggf. in Docker Desktop, ob alle 7 Container nach 10 Sekunden noch laufen."
+
+fi
+
+
+
+
+
+
